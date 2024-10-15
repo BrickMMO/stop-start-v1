1aa2dcc9cbbf618a7cf2a6e197e6b5b24f670b6c
Adam <thomasadam83@hotmail.com>
2024-10-15 07:58:37 -0400

    Removed previous version.

--- includes/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// ************************************************************
-// Initialize the session
-// 
-// The session creates a file on the server that allows PHP to 
-// to store data associated with each visitor to your web site.
-
-session_start();
